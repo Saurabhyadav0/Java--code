f780648a86c6254ed9f120d73566c3cd263e0740
Saurabh yadav <134767123+Saurabhyadav0@users.noreply.github.com>
2024-03-31 19:31:19 +0530

    Create 17-FindUnique.java

---
+++ 17-FindUnique.java
@@ -0,0 +1,22 @@
+package com.saurabh;
+
+public class FindUnique {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 3, 4, 2, 6, 4};
+        System.out.println(ans(arr));
+    }
+
+    private static int ans(int[] arr) {
+        int unique = 0;
+
+        for (int n : arr)
+        //for (int i = 0; i < arr.length; i++) {
+            //    int n = arr[i];
+            //    // Code block to be executed for each element in the array 'arr'
+            {
+            unique ^= n;
+        }
+
+        return unique;
+    }
+}
